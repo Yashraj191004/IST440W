4216de19c4c34bb82289964a20c0a9b88a2b1bf6
Dev Mehta <devm2632@gmail.com>
2026-02-05 09:18:25 -0500

    can loop through csv files

--- textExtraction/reader.py
+++ textExtraction/reader.py
@@ -1,8 +1,9 @@
 import fitz
 import re
+import pandas as pd
 
-def extract_correct_viscosity(pdf_path):
-    sae_pattern = r"\b\d{1,2}W-\d{2}\b"
+def text_extraction(pdf_path):
+    oil_pattern = r"\b\d{1,2}W-\d{2}\b"
     negative_words = ["not", "avoid", "don't", "except", "never", "other than"]
     results = []
 
@@ -12,15 +13,33 @@ def extract_correct_viscosity(pdf_path):
             sentences = text.split('.')
             for sentence in sentences:
                 clean_sentence = sentence.strip().lower()                
-                matches = re.findall(sae_pattern, sentence, re.IGNORECASE)
+                matches = re.findall(oil_pattern, sentence, re.IGNORECASE)
                 if matches:
                     is_negative = any(neg in clean_sentence for neg in negative_words)
-                    
                     if not is_negative:
                         for match in matches:
                             results.append(match)
                             
     return list(set(results))
 
+def loop_car_bank(csv_path):
+    df = pd.read_csv(csv_path)
+    results = []
+    for index, row in df.iterrows():
+        file_path = row['path']
+        try:
+            data = text_extraction(file_path)
+            results.append(", ".join(data) if isinstance(data, list) else data)
+            print(data)
+        except Exception as e:
+            print(e)
+            results.append("Error")
+    df['viscosity'] = results
+    df.to_csv(csv_path, index=False)
+
 #test
-print(extract_correct_viscosity("manual1.pdf"))
+def main():
+    loop_car_bank("manuals.csv")
+
+if __name__ == "__main__":
+    main()
